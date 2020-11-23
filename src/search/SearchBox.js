f5040dec931ebe74e6d3e88d1111a0f59cb2a0f5
Ionut V <67919936+ionutMan07@users.noreply.github.com>
2020-11-23 18:27:09 +0200

    added savedMovies.js and searchBox.css

--- src/search/SearchBox.js
+++ src/search/SearchBox.js
@@ -3,60 +3,72 @@ import { Button, TextField } from '@material-ui/core'
 import AddIcon from '@material-ui/icons/Add'
 import { searchMovies } from '../shared/API'
 
-import styles from './SearchBox'
+import './SearchBox.css'
 
 const MovieList = (props) => {
   return (
-    <div>
-      <ul>
+    <div className='main listItem'>
+      <ul className='main'>
         {props.movies.map((movie) => (
-          <li className={styles.listItem} key={movie.id}>
+          <li className="listItem" key={movie.id}>
             <b>{movie.title}</b> ({movie.release_date})
+            <div>
+              <img src={`http://image.tmdb.org/t/p/w300/${movie.poster_path}`} alt='logo' />
+              </div>
             <Button
-              className="add_movie"
+              className="Add Movie"
               color="secondary"
               onClick={(e) => {
-                e.preventDefault()
-                props.onMovieAdd(movie)
-              }}>
-              <AddIcon /> Add movie
+                e.preventDefault();
+                props.onMovieAdd(movie);
+              }}
+            >
+              <AddIcon /> Add Movie
             </Button>
           </li>
         ))}
       </ul>
     </div>
-  )
-}
+  );
+};
 
 const SearchBox = (props) => {
-  const [term, setTerm] = useState('')
-  const [movies, setMovies] = useState([])
+  const [term, setTerm] = useState('');
+  const [movies, setMovies] = useState([]);
 
   const localMovieAdd = (movie) => {
-    setMovies([])
-    props.onMovieAdd(movie)
-  }
+    setMovies([]);
+    props.onMovieAdd(movie);
+  };
+
   return (
-    <div className={styles.main}>
+    <div>
       <TextField
-        label="Search for a movie"
         variant="outlined"
+        label="Search for a movie"
         value={term}
         onChange={(e) => {
-          setTerm(e.target.value)
+          setTerm(e.target.value);
         }}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter') {
+          searchMovies(term).then((res) => setMovies(res.data.results))
+          }
+        }
+      }
       />
       <Button
         variant="contained"
         color="primary"
         onClick={() =>
           searchMovies(term).then((res) => setMovies(res.data.results))
-        }>
+        }
+      >
         Search
       </Button>
       <MovieList movies={movies} onMovieAdd={localMovieAdd} />
     </div>
-  )
-}
+  );
+};
 
-export default SearchBox
+export default SearchBox;
